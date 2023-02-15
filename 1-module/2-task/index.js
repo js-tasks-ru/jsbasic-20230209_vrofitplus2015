5bf6a9ebf623cf5044ede6fd8ced4670542643fe
Vladimir Prokopneko <vrofitplus2015@mail.ru>
2023-02-15 13:31:48 +0700

    не могу понять, почему ошибка во второй задаче, если null возвращает false. В условиях задачи про это не было отдельно сказано

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,16 +1,15 @@
-/**
- * Эту функцию трогать не нужно
- */
+
 function print(text) {
   console.log(text);
 }
 
-/**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
- */
+
 function isValid(name) {
-  // ваш код...
+  if(name.length >= 4 && !(name.includes(' ')) && !(name == null)) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function sayHello() {
@@ -22,3 +21,6 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+
+
+sayHello();
