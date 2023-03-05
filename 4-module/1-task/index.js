c4cdbbb90143a9300e3314f468a0a716b4328674
Vladimir Prokopneko <vrofitplus2015@mail.ru>
2023-03-05 12:06:59 +0700

    4 модуль 1 и 2 задача

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.createElement('UL');
+
+  
+  for(let friend of friends) {
+      
+      let li = document.createElement('LI');
+      list.appendChild(li);
+      li.innerHTML = `${friend.firstName} ${friend.lastName}`;
+  }
+
+  return list
 }
