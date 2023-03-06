bb921d233b7e46c8115219cca03b097d9c389401
Vladimir Prokopneko <vrofitplus2015@mail.ru>
2023-03-06 13:21:22 +0700

    4 модуль 3 задача

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,30 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+   
+  let rows = table.rows[i];
+
+ 
+
+       if(rows.cells[3].dataset.available == 'true') {
+           rows.cells[3].parentNode.classList.add('available')
+
+        } else if(rows.cells[3].dataset.available == 'false') {
+          rows.cells[3].parentNode.classList.add('unavailable')
+
+        } else if(!(rows.cells[3].dataset.available == 'null')) {
+          rows.cells[3].parentNode.hidden = true;
+        }
+
+        if(rows.cells[2].innerHTML == 'm') {
+          rows.cells[2].parentNode.classList.add('male')
+        } else if(rows.cells[2].innerHTML == 'f') {
+          rows.cells[2].parentNode.classList.add('female')
+        }
+
+
+        if(rows.cells[1].innerHTML < 18) {
+          rows.style = "text-decoration: line-through";
+        }
+       
+  }
 }
